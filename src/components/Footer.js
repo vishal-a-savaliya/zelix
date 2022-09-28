6cdd8a116436a6e629aead125006ad3334afdacf
vishalsavaliya <vishandsavaliya@gmail.com>
2022-09-28 23:33:57 +0530

    edited footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -158,20 +158,20 @@ export const Footer = () => {
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="/" rel="noopener noreferrer" target="_blank" className="text-primary transition hover:text-[#23e6a87c]">
+                                        <a href="https://twitter.com/techzelix" rel="noopener noreferrer" target="_blank" className="text-primary transition hover:text-[#23e6a87c]">
                                             <span className="sr-only">Twitter</span>
                                             <svg xmlns="http://www.w3.org/2000/svg" width="23" className="w-5 h-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="https://www.linkedin.com/in/zelix-tech-62600b250" rel="noopener noreferrer" target="_blank" className="text-primary transition hover:text-[#23e6a87c]">
+                                        <a href="https://www.linkedin.com/company/zelix-tech/" rel="noopener noreferrer" target="_blank" className="text-primary transition hover:text-[#23e6a87c]">
                                             <span className="sr-only">linked In</span>
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 24 24" width="24" fill="currentColor"><path d="M15 11.13v3.697h-2.143v-3.45c0-.866-.31-1.457-1.086-1.457-.592 0-.945.398-1.1.784-.056.138-.071.33-.071.522v3.601H8.456s.029-5.842 0-6.447H10.6v.913l-.014.021h.014v-.02c.285-.44.793-1.066 1.932-1.066 1.41 0 2.468.922 2.468 2.902zM6.213 5.271C5.48 5.271 5 5.753 5 6.385c0 .62.466 1.115 1.185 1.115h.014c.748 0 1.213-.496 1.213-1.115-.014-.632-.465-1.114-1.199-1.114zm-1.086 9.556h2.144V8.38H5.127v6.447z"></path><path d="M4 2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H4zm0-2h12a4 4 0 0 1 4 4v12a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4z"></path></svg>
                                         </a>
                                     </li>
 
                                     <li>
-                                        <a href="/" rel="noopener noreferrer" target="_blank" className="text-primary transition hover:text-[#23e6a87c]">
+                                        <a href="https://dribbble.com/zelixtech" rel="noopener noreferrer" target="_blank" className="text-primary transition hover:text-[#23e6a87c]">
                                             <span className="sr-only">Dribbble</span>
                                             <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                                 <path fillRule="evenodd" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z" clipRule="evenodd" />
