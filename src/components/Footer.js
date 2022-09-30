589c791ccc723aea6ef91a67ec918b15b53ec61a
vishalsavaliya <vishandsavaliya@gmail.com>
2022-09-30 14:47:58 +0530

    updated twitter link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -158,7 +158,7 @@ export const Footer = () => {
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="https://twitter.com/techzelix" rel="noopener noreferrer" target="_blank" className="text-primary transition hover:text-[#23e6a87c]">
+                                        <a href="https://twitter.com/ZelixTech" rel="noopener noreferrer" target="_blank" className="text-primary transition hover:text-[#23e6a87c]">
                                             <span className="sr-only">Twitter</span>
                                             <svg xmlns="http://www.w3.org/2000/svg" width="23" className="w-5 h-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
                                         </a>
